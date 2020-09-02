532a51db79d3a15c28c9c3cae2c9461164756ef4
amobeeb <amobeeb1net@gmail.com>
2020-09-02 01:05:05 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <title>BotMan Widget</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+</head>
+<body>
+
+
+
+<script>
+var botmanWidget = {
+    chatServer: '/botmans',
+    desktopHeight:100%,
+    desktopWidth:100%
+};
+</script>
+<script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+</body>
+</html>
